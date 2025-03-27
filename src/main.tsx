a2527bdfd4920079d1e46330c79a8af3b0e8d1b5
LuoKing <luoking@creatify.ai>
2025-03-27 22:52:25 +0800

    feat: update

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <div className='text-body-sm bg-red-700 te'>Hello LuoKing</div>
-  </StrictMode>,
+    <StrictMode>
+        <div className='text-body-sm bg-red-700 te'>Hello LuoKing</div>
+    </StrictMode>,
 )
