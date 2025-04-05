d0faf97435c7cfa20325b5d01a7e26f2ace55b44
LuoKing <luoking@creatify.ai>
2025-04-06 00:35:35 +0800

    feat: init shadcn

--- src/common/component/side-bar-layout.tsx
+++ src/common/component/side-bar-layout.tsx
@@ -2,21 +2,25 @@ import { Link } from "@tanstack/react-router";
 import clsx from "clsx";
 import { PropsWithChildren } from "react";
 
+import { Button } from "@/component/ui/button";
+
 export function SideBarLayout(props: PropsWithChildren) {
   return (
     <div className='size-full flex'>
       <div className={clsx(
-        'w-[280px] h-full shrink-0 border-0 border-r-[1px] border-solid border-black/10',
+        'w-[280px] h-full shrink-0 border-0 border-r-[1px] border-solid border-black/10 bg-[#fafafa]',
         'flex flex-col gap-2 p-4'
       )}>
         <Link
-          className='flex justify-center items-center p-4 rounded-lg bg-purple-600 text-white hover:bg-purple-400 duration-150 cursor-pointer'
           to='/home'
-        >Create</Link>
+        >
+          <Button>Create</Button>
+        </Link>
         <Link
-          className='flex justify-center items-center p-4 rounded-lg bg-purple-600 text-white hover:bg-purple-400 duration-150 cursor-pointer'
           to='/projects'
-        >Projects</Link>
+        >
+          <Button>Project</Button>
+        </Link>
       </div>
       <div className='flex-1 w-0 h-full'>{props.children}</div>
     </div>
