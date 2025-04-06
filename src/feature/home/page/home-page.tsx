73fbfcf9efc4f330859727a906d1b0cc4cc2d40e
LuoKing <luoking@creatify.ai>
2025-04-06 12:32:48 +0800

    feat: update style

--- src/feature/home/page/home-page.tsx
+++ src/feature/home/page/home-page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/component/ui/button";
 
 export function HomePage() {
   return (
-    <div className='size-full '>
+    <div className='size-full p-5'>
       <Link to='/editor'>
         <Button>Go to editor</Button>
       </Link>
