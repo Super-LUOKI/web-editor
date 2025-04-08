feb7b3fab64d22cb3b97fc5f82a183213c214e21
LuoKing <luoking@creatify.ai>
2025-04-09 00:09:22 +0800

    feat: update schema

--- src/lib/remotion/editor-render/schema.ts
+++ src/lib/remotion/editor-render/schema.ts
@@ -2,13 +2,16 @@ import { z } from "zod";
 
 export const TimelineSchema = z.object({});
 
-export const OutputSchema = z.object({})
+export const MetaSchema = z.object({
+  watermark: z.string().optional(),
+  thumbnailFrame: z.number().optional(),
+  width: z.number().optional(),
+  height: z.number().optional(),
+  fps: z.number().optional(),
+})
 
 export const DraftDataSchema = z.object({
   name: z.string().optional(),
   timeline: TimelineSchema,
-  output: OutputSchema,
-  acceptAspectRatios: z.array(z.string()).optional(),
-  watermark: z.boolean().optional(),
-  thumbnailFrame: z.number().optional(),
+  meta: MetaSchema,
 });
