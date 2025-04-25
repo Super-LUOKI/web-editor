92160d20e7e5d91062b74c55d90ae5076f6e34ec
LuoKing <luoking@creatify.ai>
2025-04-25 22:12:24 +0800

    feat: register element to render context

--- src/lib/remotion/editor-render/component/visual-container.tsx
+++ src/lib/remotion/editor-render/component/visual-container.tsx
@@ -1,34 +1,46 @@
-import { CSSProperties, PropsWithChildren, useMemo } from "react";
+import { CSSProperties, PropsWithChildren, useMemo, useRef } from "react";
 import { useCurrentFrame, useVideoConfig } from "remotion";
 
 import { AnimationFactory } from "../animation/animation-factory.ts";
+import { useRegisterBox } from "../render-context.tsx";
 import { DisplayElement } from "../schema/element.ts";
 import { getAttributeWithOverwrite } from "../utils/draft.ts";
 
 type VisualContainerProps = PropsWithChildren<{
     element: DisplayElement
 }>
-export function VisualContainer(props: VisualContainerProps){
+
+export function VisualContainer(props: VisualContainerProps) {
+  const { element } = props
+
+  const ref = useRef<HTMLDivElement | null>(null)
   const { fps } = useVideoConfig()
   const currentFrame = useCurrentFrame()
-  const { element } = props
-    
-  const animation = useMemo(()=>AnimationFactory.createAnimation(element), [element])
 
-  const animAttribute = useMemo(()=>{
+  const animation = useMemo(() => AnimationFactory.createAnimation(element), [element])
+
+  const animAttribute = useMemo(() => {
     return animation?.getAnimationProperty(currentFrame / fps - element.start)
   }, [animation, currentFrame, fps])
-    
-    
+
+
   const elementX = getAttributeWithOverwrite(element, 'x', animAttribute, 0)
   const elementY = getAttributeWithOverwrite(element, 'y', animAttribute, 0)
   const elementRotate = getAttributeWithOverwrite(element, 'rotate', animAttribute, 0)
   const elementScaleX = getAttributeWithOverwrite(element, 'scaleX', animAttribute, 1)
   const elementScaleY = getAttributeWithOverwrite(element, 'scaleY', animAttribute, 1)
   const elementOpacity = getAttributeWithOverwrite(element, 'opacity', animAttribute, 1)
-    
+
+  useRegisterBox({
+    id: element.id,
+    ref,
+    parent: element.parent,
+    children: element.children
+  })
+
 
   return <div
+    ref={ref}
     style={{
       width: element.width || 'max-content',
       height: element.height || 'max-content',
--- src/lib/remotion/editor-render/render-context.tsx
+++ src/lib/remotion/editor-render/render-context.tsx
@@ -1,8 +1,10 @@
-import { createContext, PropsWithChildren, RefObject, useContext } from "react";
+import { createContext, PropsWithChildren, RefObject, useContext, useLayoutEffect } from "react";
+
+export type BoxRegisterParams = { parent?: string; children?: string[], ref?: RefObject<HTMLElement | null> }
 
 export type RenderContextValue = {
     /** { [elementId]: {parent: ParentElementId, children: ChildElementId[], ref: ElementDomRef} } */
-    box: Partial<Record<string, { parent?: string; children?: string[], ref?: RefObject<HTMLElement | undefined> }>>,
+    box: Partial<Record<string, BoxRegisterParams>>,
 }
 
 const RenderContext = createContext<RenderContextValue | null>(null)
@@ -24,4 +26,26 @@ export const useDraftRenderContext = () => {
     throw new Error("useDraftRenderContext must be used within a DraftRenderProvider")
   }
   return context
+}
+
+export function useRegisterBox(params: BoxRegisterParams & {id: string}){
+  const { id, ref, parent, children } = params;
+  const context = useDraftRenderContext();
+    
+  useLayoutEffect(()=>{
+    if (!context) return;
+
+    if (context.box[id]) {
+      console.warn(`box already registered: ${id}`)
+    }
+    
+    context.box[id] = {
+      ref,
+      parent,
+      children,
+    }
+    return () => {
+      delete context.box[id]
+    }
+  }, [])
 }
